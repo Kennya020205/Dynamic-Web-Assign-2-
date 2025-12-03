ccadfc46340f3b1bb205ca1e9ad65457a0c7435b
Kennya020205 <Kendrick.yalung@gmail.com>
2025-12-03 21:30:07 +0800

    Enhance header with navigation links
    Added a header section with navigation links.

--- header.php
+++ header.php
@@ -6,4 +6,13 @@
 </head>
 <body>
 
-<h1>Pet Supplies Shop</h1>
+<header>
+    <h1>Pet Supplies Shop</h1>
+    <nav class="navbar">
+        <a href="index.php" class="nav-link">Home</a>
+        <a href="functions.php" class="nav-link">Stock Monitor</a>
+        <a href="#" class="nav-link">Food</a>
+        <a href="#" class="nav-link">Treats</a>
+        <a href="#" class="nav-link">Toys</a>
+    </nav>
+</header>
